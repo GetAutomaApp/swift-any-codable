3a931e149d74a27dbd5f33b1374336dc78705881
AdonisCodes <business@simonferns.com>
2025-05-03 09:20:07 -0600

    fix: formatting

--- Package.swift
+++ Package.swift
@@ -3,18 +3,18 @@ import PackageDescription
 
 let package = Package(
     name: "swift-any-codable",
-	platforms: [
-		.iOS(.v13),
-		.macOS(.v10_15),
-		.tvOS(.v13),
-	],
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+    ],
     products: [
-        .library(name: "AnyCodable",targets: ["AnyCodable"]),
+        .library(name: "AnyCodable", targets: ["AnyCodable"]),
     ],
     targets: [
         .target(
-			name: "AnyCodable"
-		),
+            name: "AnyCodable"
+        ),
         .testTarget(
             name: "AnyCodableTests",
             dependencies: ["AnyCodable"]
